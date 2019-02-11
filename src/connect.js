fec7a7b609c7fb0634c7cbbe81a2a09a2cd20100
chao.qu03 <chao.qu03@ele.me>
2019-02-11 15:39:13 +0800

    Fix state update causes view exception refresh bugs

--- src/connect.js
+++ src/connect.js
@@ -9,30 +9,35 @@ export default function (Component, contextArgs) {
       this.contexts = {};
     }
 
-    renderContext(ctxs) {
-      const CtxName = ctxs.shift();
-      if (CtxName) {
-        if (!stores.data[CtxName]) {
-          throw new Error(`KV Store:\n Uncaught ReferenceError: ${CtxName} Context is not defined.`);
-        }
-        const Ctx = stores.data[CtxName];
-        return (
-          <Ctx.visitor.ctx.Consumer>
-            {
-              (data) => {
-                this.contexts[CtxName] = data;
-                return this.renderContext(ctxs);
-              }
-            }
-          </Ctx.visitor.ctx.Consumer>
-        );
-      } else {
-        if (this.props.store !== undefined || this.props.setStore !== undefined) {
-          throw new Error('KV Store:\n Error: Keyword store and setStore are reserved words. Check your props and replace them with other keys.');
-        }
+    renderContext(ctxs, cacheLen) {
+      const CtxLen = (typeof cacheLen === 'number' && !isNaN(cacheLen)) ? cacheLen : ctxs.length - 1;
+      const CtxName = ctxs[CtxLen];
 
-        return <Component setStore={setStore} store={this.contexts} {...this.props} />;
-      }
+      return ((ctxname, len) => {
+        if (ctxname && len >= 0) {
+          if (!stores.data[ctxname]) {
+            throw new Error(`KV Store:\n Uncaught ReferenceError: ${CtxName} Context is not defined.`);
+          }
+
+          const Ctx = stores.data[ctxname];
+          return (
+            <Ctx.visitor.ctx.Consumer name={ctxname}>
+              {
+                (data) => {
+                  this.contexts[CtxName] = data;
+                  return this.renderContext(ctxs, len - 1);
+                }
+              }
+            </Ctx.visitor.ctx.Consumer>
+          );
+        } else {
+          if (this.props.store !== undefined || this.props.setStore !== undefined) {
+            throw new Error('KV Store:\n Error: Keyword store and setStore are reserved words. Check your props and replace them with other keys.');
+          }
+          
+          return <Component setStore={setStore} store={this.contexts} {...this.props} />;
+        }
+      })(CtxName, CtxLen);
     }
 
     render() {
